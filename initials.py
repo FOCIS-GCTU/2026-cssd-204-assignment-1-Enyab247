6e71abf9b167ec0a3e1c4b4e84b56fba890be191
Enyab247 <addaebonsue@gmail.com>
2026-06-01 23:21:26 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: EDWARD ADDAE BONSU NANA YAW
+# STUDENT ID: 2425403149
+# Email: addaebonsue@gmail.com
+# Grader: Carolyn or Emma or Ahmad
+#
+# On my honor, EDWARD NANA YAW ADDAE BONSU, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+    # Prints my initials
 
+    print()
+    print("...EABNY")
+    print()
+
+    print("...EEEEEEEEEEE...........AAAA............BBBBBBBBBB..........NNNN.........NN........YY........YY...")
+    print("...EE...................AA..AA...........BB.......BB.........NN.NN........NN.........YY......YY....")
+    print("...EE..................AA....AA..........BB.......BBB........NN..NN.......NN..........YY....YY.....")
+    print("...EE.................AA......AA.........BB.......BB.........NN...NN......NN...........YY..YY......")
+    print("...EEEEEEEEE.........AAAAAAAAAAAA........BBBBBBBBBB..........NN....NN.....NN............YYYY.......")
+    print("...EE................AA........AA........BB.......BB.........NN.....NN....NN.............YY........")
+    print("...EE................AA........AA........BB.......BBB........NN......NN...NN.............YY........")
+    print("...EE................AA........AA........BB.......BBB........NN.......NN..NN.............YY........")
+    print("...EE............**..AA........AA...**...BBBBBBBBBBBB...**...NN........NN.NN...**........YY........")
+    print("...EEEEEEEEEEE...**..AA........AA...**...BBBBBBBBBBB....**...NN.........NNNN...**........YY........")
+
+    print()
 
 
 main()
